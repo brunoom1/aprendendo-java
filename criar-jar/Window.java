35a0e561926b7065dcd738c2764b351cee66932f
Gabriel Mendonça <gmendonca@fingerprint.com.br>
2019-11-06 06:40:38 -0300

    Criar executável win.jar que levanta uma janela awt

---
+++ criar-jar/Window.java
@@ -0,0 +1,57 @@
+
+import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Window extends Frame{
+	
+	public static void main (String args[]) {
+		Window win = new Window();
+		win.setSize(400, 400);
+		win.addWindowListener(new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowIconified(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeiconified(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeactivated(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				System.exit(0);								
+			}
+			
+			@Override
+			public void windowClosed(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+			}
+			
+			@Override
+			public void windowActivated(WindowEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		win.setVisible(true);
+	}
+	
+}
